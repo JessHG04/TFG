8cd74fa7bcc8d2b2a9ffa548b30ee003863f14aa
Jessica <jessicahg04@gmail.com>
2022-06-22 19:44:18 +0200

    Gestion eliminación entidades

--- Proyecto Unity TFG/Assets/Scripts/LevelGenerator.cs
+++ Proyecto Unity TFG/Assets/Scripts/LevelGenerator.cs
@@ -6,21 +6,31 @@ public class LevelGenerator : MonoBehaviour {
     public GameObject platformGO;
     [SerializeField] private Transform platformStart;
     [SerializeField] private Transform platform_1;
+    private const float platformDestroyPositionX = -50f;
     private List<Platform> platformList;
+    private Vector3 lastEndPosition;
 
     private void Awake() {
         Transform lastPlatformTransf;
+        lastEndPosition = platformStart.Find("EndPosition").position;
         platformList = new List<Platform>();
-        lastPlatformTransf =  SpawnPlatform(platformStart.Find("EndPosition").position);
+        platformList.Add((GameObject.Find("Platform_Start")).GetComponent<Platform>());
+        
+        lastPlatformTransf = SpawnPlatform(platformStart.Find("EndPosition").position);
         lastPlatformTransf.position = lastPlatformTransf.position + new Vector3(10, 1, 0);
-        lastPlatformTransf =  SpawnPlatform(lastPlatformTransf.Find("EndPosition").position);
+        lastPlatformTransf = SpawnPlatform(lastPlatformTransf.Find("EndPosition").position);
         lastPlatformTransf.position = lastPlatformTransf.position + new Vector3(10, 1, 0);
-        lastPlatformTransf =  SpawnPlatform(lastPlatformTransf.Find("EndPosition").position);
+        lastPlatformTransf = SpawnPlatform(lastPlatformTransf.Find("EndPosition").position);
         lastPlatformTransf.position = lastPlatformTransf.position + new Vector3(10, 1, 0);
-        lastPlatformTransf =  SpawnPlatform(lastPlatformTransf.Find("EndPosition").position);
+        lastPlatformTransf = SpawnPlatform(lastPlatformTransf.Find("EndPosition").position);
         lastPlatformTransf.position = lastPlatformTransf.position + new Vector3(10, 1, 0);
-        lastPlatformTransf =  SpawnPlatform(lastPlatformTransf.Find("EndPosition").position);
+        lastPlatformTransf = SpawnPlatform(lastPlatformTransf.Find("EndPosition").position);
         lastPlatformTransf.position = lastPlatformTransf.position + new Vector3(10, 1, 0);
+    }
+
+    private void SpawnPlatform() {
+        Transform lastPlatformTransf = SpawnPlatform(lastEndPosition);
+        lastEndPosition = lastPlatformTransf.Find("EndPosition").position;
     }
 
     private Transform SpawnPlatform(Vector3 spawnPosition) {
@@ -35,8 +45,14 @@ public class LevelGenerator : MonoBehaviour {
     }
 
     private void UpdatePlatforms() {
-        foreach (Platform platform in platformList) {
-            platform.Move();
+        for(int x = 0; x < platformList.Count; x++){
+            if(platformList[x].getPlatformTransform().position.x <= platformDestroyPositionX){
+                platformList[x].DestroySelf();
+                platformList.Remove(platformList[x]);
+            }else{
+                platformList[x].Move();
+            }
+            
         }
     }
 }
