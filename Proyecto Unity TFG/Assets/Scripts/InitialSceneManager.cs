35dcbfdd4bffe0ccdc8d5266d24a2af89a94934b
Jessica <jessicahg04@gmail.com>
2022-07-12 01:00:00 +0200

    Desarrollo avanzado hasta Ninja Run

--- Proyecto Unity TFG/Assets/Scripts/InitialSceneManager.cs
+++ Proyecto Unity TFG/Assets/Scripts/InitialSceneManager.cs
@@ -5,35 +5,22 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class InitialSceneManager : MonoBehaviour {
-    //public int target = 60;
-      
-    private void Awake() {
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate = target;
-        //Screen.SetResolution(1920, 1080, FullScreenMode.ExclusiveFullScreen, 60);
-    }
-      
+          
     private void Update() {
-        /*if(Application.targetFrameRate != target){
-            Application.targetFrameRate = target;
-        }*/
         if(Input.GetKey(KeyCode.Escape)){
             Application.Quit();
         }
     }
     
     public void NinjaRunClicked() {
-        //Play Sound Effect
         SceneManager.LoadScene("NinjaRun", LoadSceneMode.Single);
     }
 
     public void HigherLowerClicked() {
-        //Play Sound Effect
         SceneManager.LoadScene("HigherLower", LoadSceneMode.Single);
     }
 
     public void ExitButtonClicked() {
-        //Play Sound Effect
         Application.Quit();
     }
 }
