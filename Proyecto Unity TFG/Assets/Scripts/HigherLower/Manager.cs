ea1e0ecdab0defa87834a965dc8a10f254bb7660
Jessica <jessicahg04@gmail.com>
2022-07-18 23:41:57 +0200

    Notas y vibracion

--- Proyecto Unity TFG/Assets/Scripts/HigherLower/Manager.cs
+++ Proyecto Unity TFG/Assets/Scripts/HigherLower/Manager.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
@@ -5,6 +6,8 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour {
     private static Manager instance;
+    private int round = 0;
+    private int maxRounds;
     private int score = 0;
     private int oldHighScore = 0;
     [SerializeField] private List<Sound> soundsList;
@@ -15,13 +18,105 @@ public class Manager : MonoBehaviour {
 
     private void Awake() {
         instance = this;
+        Vibration.Init();
+        maxRounds = soundsList.Count - 1;
+        Debug.Log(maxRounds);
         //oldHighScore = PlayerPrefs.GetInt("Highscore");
+        shuffle();
+        playFirstSound();
     }
 
-    private void Update() {
-        for (int x = 0; x<soundsList.Count; x++){
-            soundsList[x].playSound();
+    private void shuffle() {
+        int randomIndex = Random.Range(0, soundsList.Count -1);
+        Sound snd;
+        int it = 0;
+        
+        while(it < 2){
+            for(int x = 0; x < soundsList.Count; x++){
+                snd = soundsList[x];
+                soundsList[x] = soundsList[randomIndex];
+                soundsList[randomIndex] = snd;
+            }
+            it++;
         }
     }
 
+    public void HighClicked() {
+        int nextRound = round + 1;
+        if(soundsList[nextRound].getFreq() > soundsList[round].getFreq()){
+            Debug.Log("Acierto");
+            score++;
+        }
+        else{
+            Debug.Log("Fallo");
+            Vibration.Cancel();
+            returnToMenu();
+        }
+        round++;
+        if(round < maxRounds){
+            playFirstSound();
+        }
+        else{
+            Debug.Log("Fin del juego");
+            Vibration.Cancel();
+            returnToMenu();
+        }
+    }
+
+    public void LowClicked() {
+        int nextRound = round + 1;
+        if(soundsList[nextRound].getFreq() < soundsList[round].getFreq()){
+            Debug.Log("Acierto");
+            score++;
+        }
+        else{
+            Debug.Log("Fallo");
+            Vibration.Cancel();
+            returnToMenu();
+        }
+        round++;
+        if(round < maxRounds){
+            playFirstSound();
+        }
+        else{
+            Debug.Log("Fin del juego");
+            Vibration.Cancel();
+            returnToMenu();
+        }
+    }
+
+    private void playFirstSound() {
+        int ms = soundsList[round].getFreq();
+        int sec = ms / 1000;
+        Vibration.Vibrate(ms); //Miliseconds
+        soundsList[round].playSound();
+        Invoke("playSecondSound", sec); //Seconds
+    }
+
+    private void playSecondSound() {
+        Vibration.Vibrate(soundsList[round].getFreq()); //Miliseconds
+        soundsList[round + 1].playSound();
+    }
+
+    /*private void playEndGame() {
+        playAudio.Pause();
+        if(score > oldHighScore){
+            Invoke("returnToMenu", 2.0f);
+            if(!playAudio.isPlaying){
+                playAudio.clip = newHighscore;
+                playAudio.Play();
+            }
+        }
+        else{
+            Invoke("returnToMenu", 2.75f);
+            if(!playAudio.isPlaying){
+                playAudio.clip = noHighscore;
+                playAudio.Play();
+            }
+        }
+    }*/
+
+    private void returnToMenu() {
+        SceneManager.LoadScene("InitialScene", LoadSceneMode.Single);
+    }
 }
--- Proyecto Unity TFG/Assets/Scripts/HigherLower/Sound.cs
+++ Proyecto Unity TFG/Assets/Scripts/HigherLower/Sound.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Sound : MonoBehaviour {
     [SerializeField] private AudioSource sound;
+    [SerializeField] private int freq;
+
+    public int getFreq() => freq;
+    //public int getSoundLength() => Mathf.RoundToInt(sound.clip.length);
 
     public void playSound() {
         //sound.PlayOneShot(sound.clip);
-        Debug.Log("playSound");
+        //Debug.Log("playSound");
         sound.PlayOneShot(sound.clip);
     }
 }
